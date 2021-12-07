ef505cda4181246a8f0db69ee4f0fa08353413db
谢克 <1023052645@qq.com>
2021-12-08 00:22:19 +0800

    add snowflake config

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,7 +1,14 @@
-use id_generator::snowflake;
+use chrono::{TimeZone, Utc};
+use id_generator::snowflake::{self, SnowflakeConfig};
 
 fn main() {
-    snowflake::set_config(0, 0);
+    let mut config = SnowflakeConfig::new();
+    config.base_timestamp = Utc.ymd(2021, 1, 1).and_hms(0, 0, 0).timestamp_millis();
+    config.datacenter_id = 0;
+    config.worker_id = 0;
+    // ...
+    snowflake::set_config(config);
+
     let id = snowflake::next_id();
     println!("{}", id);
 }
--- src/snowflake.rs
+++ src/snowflake.rs
@@ -7,64 +7,26 @@ use std::sync::{Arc, Mutex};
 
 #[derive(Debug, Default)]
 struct Snowflake {
-    epoch: i64,
-    datacenter_id: i64,
-    worker_id: i64,
     sequence: i64,
     last_timestamp: i64,
-
-    datacenter_id_bits: u8,
-    worker_id_bits: u8,
-    sequence_bits: u8,
-
-    timestamp_left_shift: u8,
-    datacenter_id_left_shift: u8,
-    worker_id_left_shift: u8,
+    config: SnowflakeConfig,
 }
 
 impl Snowflake {
-    pub fn new(datacenter_id: i64, worker_id: i64) -> Self {
-        let mut snowflake = Snowflake::default();
-        snowflake.epoch = Utc.ymd(2021, 1, 1).and_hms(0, 0, 0).timestamp_millis();
-        snowflake.init(5, 5, 12);
-        snowflake.set_datacenter_id(datacenter_id);
-        snowflake.set_worker_id(worker_id);
-        snowflake
+    pub fn new() -> Self {
+        Snowflake::default()
     }
 
-    fn set_datacenter_id(&mut self, datacenter_id: i64) {
-        let mask = -1_i64 << self.datacenter_id_bits;
-        if datacenter_id & mask != 0 {
-            panic!("invalid datacenter_id {}.", datacenter_id);
+    pub fn set_config(&mut self, config: SnowflakeConfig) {
+        if config.is_valid() {
+            self.config = config;
+        } else {
+            panic!("invalid snowflake config!")
         }
-        self.datacenter_id = datacenter_id;
-    }
-
-    fn set_worker_id(&mut self, worker_id: i64) {
-        let mask = -1_i64 << self.worker_id_bits;
-        if worker_id & mask != 0 {
-            panic!("invalid worker_id {}.", worker_id);
-        }
-        self.worker_id = worker_id;
-    }
-
-    fn set_config(&mut self, datacenter_id: i64, worker_id: i64) {
-        self.set_datacenter_id(datacenter_id);
-        self.set_worker_id(worker_id);
-    }
-
-    fn init(&mut self, datacenter_id_bits: u8, worker_id_bits: u8, sequence_bits: u8) {
-        self.datacenter_id_bits = datacenter_id_bits;
-        self.worker_id_bits = worker_id_bits;
-        self.sequence_bits = sequence_bits;
-
-        self.timestamp_left_shift = self.datacenter_id_bits + self.worker_id_bits + self.sequence_bits;
-        self.datacenter_id_left_shift = self.worker_id_bits + self.sequence_bits;
-        self.worker_id_left_shift = self.sequence_bits;
     }
 
     fn get_timestamp(&self) -> i64 {
-        Utc::now().timestamp_millis() - self.epoch
+        Utc::now().timestamp_millis() - self.config.base_timestamp
     }
 
     fn next_timestamp(&self, timestamp: i64) -> i64 {
@@ -90,8 +52,7 @@ impl Snowflake {
         }
 
         if current_timestamp == self.last_timestamp {
-            let mask = !(-1_i64 << self.sequence_bits);
-            self.sequence = (self.sequence + 1) & mask;
+            self.sequence = (self.sequence + 1) & self.config.sequence_mask();
             if self.sequence == 0 {
                 if is_back {
                     current_timestamp += 1;
@@ -105,19 +66,83 @@ impl Snowflake {
 
         self.last_timestamp = current_timestamp;
 
-        current_timestamp << self.timestamp_left_shift
-            | self.datacenter_id << self.datacenter_id_left_shift
-            | self.worker_id << self.worker_id_left_shift
+        current_timestamp << self.config.timestamp_left_shift()
+            | self.config.datacenter_id << self.config.datacenter_id_left_shift()
+            | self.config.worker_id << self.config.worker_id_left_shift()
             | self.sequence
     }
 }
 
-lazy_static! {
-    static ref SNOWFLAKE: Arc<Mutex<Snowflake>> = Arc::new(Mutex::new(Snowflake::new(0, 0)));
+#[derive(Debug)]
+pub struct SnowflakeConfig {
+    pub base_timestamp: i64,
+    pub datacenter_id: i64,
+    pub worker_id: i64,
+    pub datacenter_id_bits: u8,
+    pub worker_id_bits: u8,
+    pub sequence_bits: u8,
 }
 
-pub fn set_config(datacenter_id: i64, worker_id: i64) {
-    SNOWFLAKE.lock().unwrap().set_config(datacenter_id, worker_id)
+impl Default for SnowflakeConfig {
+    fn default() -> Self {
+        Self {
+            base_timestamp: Utc.ymd(2021, 1, 1).and_hms(0, 0, 0).timestamp_millis(),
+            datacenter_id: 0,
+            worker_id: 0,
+            datacenter_id_bits: 5,
+            worker_id_bits: 5,
+            sequence_bits: 12,
+        }
+    }
+}
+
+impl SnowflakeConfig {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn is_valid(&self) -> bool {
+        let mask = -1_i64 << self.datacenter_id_bits;
+        if self.datacenter_id & mask != 0 {
+            error!("invalid datacenter_id {}.", self.datacenter_id);
+            return false;
+        }
+
+        let mask = -1_i64 << self.worker_id_bits;
+        if self.worker_id & mask != 0 {
+            error!("invalid worker_id {}.", self.worker_id);
+            return false;
+        }
+        true
+    }
+
+    #[inline(always)]
+    fn sequence_mask(&self) -> i64 {
+        !(-1_i64 << self.sequence_bits)
+    }
+
+    #[inline(always)]
+    fn timestamp_left_shift(&self) -> u8 {
+        self.datacenter_id_bits + self.worker_id_bits + self.sequence_bits
+    }
+
+    #[inline(always)]
+    fn datacenter_id_left_shift(&self) -> u8 {
+        self.worker_id_bits + self.sequence_bits
+    }
+
+    #[inline(always)]
+    fn worker_id_left_shift(&self) -> u8 {
+        self.sequence_bits
+    }
+}
+
+lazy_static! {
+    static ref SNOWFLAKE: Arc<Mutex<Snowflake>> = Arc::new(Mutex::new(Snowflake::new()));
+}
+
+pub fn set_config(config: SnowflakeConfig) {
+    SNOWFLAKE.lock().unwrap().set_config(config)
 }
 
 pub fn next_id() -> i64 {
